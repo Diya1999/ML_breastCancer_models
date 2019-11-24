fb3c7ea7f2bebd9556c4a8c9e9a88e5cd23f28a8
Gaurav-Sangappa <39906890+Gaurav-Sangappa@users.noreply.github.com>
2019-11-24 22:20:58 +0530

    All changes made

--- ml_test2.py
+++ ml_test2.py
@@ -4,51 +4,14 @@ Created on Sat Nov 23 22:23:40 2019
 
 @author: Hp
 """
-
+import matplotlib.pyplot as plt 
 from random import randrange
 from csv import reader
-#loading the dataset 
-def load_csv(filename):
-	dataset = list()
-	with open(filename, 'r') as file:
-		csv_reader = reader(file)
-		for row in csv_reader:
-			if not row:
-				continue
-			dataset.append(row)
-	return dataset
- 
-#To calculate the groups for the proposed split in data
-def split_test(index, value, dataset):
-    left = []
-    right = []
-    for col in dataset:
-        if col[index] < value:
-            left.append(col)
-        else:
-            right.append(col)
-    return left, right
-#To calculate the gini index of a proposed split 
-def gini(groups, classes):
-    n_instances = 0 
-    for group in groups:
-        n_instances = float(n_instances + len(group))
-    gini_val = 0.0
-    for group in groups:
-        size = float(len(group))
-        if(size==0):
-            continue
-        score = 0.0
-        for class_val in classes:
-            g = ([row[-1] for row in group].count(class_val)) / size
-            score = score + (g*g)
-        gini_val = gini_val + (1.0 - score)* (size/ n_instances)
-    return gini_val 
 #calculate the optimal split
 def get_split(dataset, n_features):
     class_mid = set(row [-1] for row in dataset)
-    class_val = list(class_mid)
-    opt_index, opt_value, opt_score, opt_groups = 9999, 9999, 9999, None
+    classes = list(class_mid)
+    opt_index, opt_value, opt_score, opt_groups = 999, 999, 999, None
     feat = list()
     while (len(feat) < n_features):
         col = randrange(len(dataset[0])-1)
@@ -56,38 +19,85 @@ def get_split(dataset, n_features):
             feat.append(col)
     for col in feat:
         for row in dataset:
-            groups = split_test(col, row[col], dataset) 
-            gini_val = gini(groups, class_val)
+            groups = split_test(col, row[col], dataset)
+            n_instances = 0 
+            count = 0
+            #finding gini index 
+            for group in groups:
+                n_instances = float(n_instances + len(group))
+            gini_val_mid = 0.0
+            for group in groups:
+                size = int(len(group))
+                if(size==0):
+                    continue
+                score = 0.0
+                for class_val in classes:
+                    for row in group:
+                        if (row[-1] == class_val):
+                            count = count + 1 
+                g = count / size 
+                score = score + (g*g)
+                gini_val_mid = gini_val_mid + (1.0 - score)* (size/ n_instances)
+            gini_val = gini_val_mid 
+            #gets optimal column to split and the point at which column should be split 
             if(gini_val < opt_score):
                 opt_index, opt_value, opt_score, opt_groups = col, row[col], gini_val, groups
         return {'index':opt_index, 'value':opt_value, 'groups':opt_groups}
-#to get the most probable class of the terminal node 
-def to_terminal(group):
-	outcomes = [row[-1] for row in group]
-	return max(set(outcomes), key=outcomes.count)
+#To calculate the groups for the proposed split in data
+def split_test(index, value, dataset):
+    left = []
+    right = []
+    groups = []
+    for col in dataset:
+        if col[index] < value:
+            left.append(col)
+        else:
+            right.append(col)
+    groups.append(left)
+    groups.append(right)
+    return groups
 #to get splits of the data 
-def split(node, min_size, n_features, depth):
+def create_tree(node, min_size, n_features, depth):
     left, right = node['groups']
     del(node['groups'])
     if not left or not right:
-        node['left'] = node['right'] = to_terminal(left + right)
+        #to get the most probable class of the terminal node
+        outcomes = [row[-1] for row in (left+right)]
+        node['left'] = node['right'] = max(set(outcomes), key = outcomes.count)
         return
     if( len(left) <= min_size):
-        node['left'] = to_terminal(left)
+        #to get the most probable class of the terminal node
+        outcomes = [row[-1] for row in left]
+        node['left'] = max(set(outcomes), key = outcomes.count)
     else:
         node['left'] = get_split(left, n_features)
-        split(node['left'], min_size, n_features, depth+1)
+        create_tree(node['left'], min_size, n_features, depth+1)
     if( len(right) <= min_size):
-        node['right'] = to_terminal(right)
+        #to get the most probable class of the terminal node
+        outcomes = [row[-1] for row in (left+right)]
+        node['right'] = max(set(outcomes), key = outcomes.count)
     else:
         node['right'] = get_split(right, n_features)
-        split(node['right'], min_size, n_features, depth+1)
+        create_tree(node['right'], min_size, n_features, depth+1)
 
-def build_tree(train, min_size, n_features):
-    root = get_split(train, n_features)
-    split(root, min_size, n_features, 1)
-    return root
-    
+def random_forests(train, test, min_size, n_trees, n_features):
+    forest = list()
+    predictions =  []
+    for i in range(n_trees):
+        tree = get_split(train, n_features)
+        create_tree(tree, min_size, n_features, 1)
+        forest.append(tree)
+    for row in test:
+        predictions_mid = []
+        for tree in forest:
+            prediction = predict(tree,row)
+            predictions_mid.append(prediction)
+    #print(predictions)
+        max_prediction = max(set(predictions_mid), key = predictions_mid.count)
+        prediction = max_prediction
+        predictions.append(prediction)
+    #print(predictions)
+    return(predictions)
 def predict(node, row):
     #print(node)
     #print(node['index'])
@@ -102,58 +112,20 @@ def predict(node, row):
             return predict(node['right'], row)
         else:
             return node['right']
-
-def sampling(dataset, ratio):
-    sample = list()
-    size = round(len(dataset)*ratio)
-    while(len(sample) < size ):
-        random_pick = randrange(len(dataset))
-        sample.append(dataset[random_pick])
-    return sample
-def random_forest_prediction(trees, row):
-    predictions = []
-    for tree in trees:
-        #print(tree)
-        prediction = predict(tree,row)
-        predictions.append(prediction)
-    max_prediction = max(set(predictions), key = predictions.count)
-    return (max_prediction) 
-def create_random_forests(train, test, min_size, sample_size, n_trees, n_features):
-    forest = list()
-    predictions =  []
-    for i in range(n_trees):
-        sample = sampling(train, sample_size)
-        tree = build_tree(sample, min_size, n_features)
-        print(i)
-        print('\n')
-        print(tree)
-        forest.append(tree)
-    for row in test:
-        prediction = random_forest_prediction(forest, row)
-        predictions.append(prediction)
-    #print(predictions)
-    return(predictions)
-def accuracy_metric(actual, predicted):
-    correct = 0
-    #print(predicted)
-    for i in range(len(actual)):
-        if (actual[i] == predicted[i]):
-            correct += 1
-    return correct / float(len(actual)) * 100.0
-def cross_validation_split(dataset, n_folds):
-	dataset_split = list()
-	dataset_copy = list(dataset)
-	fold_size = int(len(dataset) / n_folds)
-	for j in range(n_folds):
-		fold = list()
-		while len(fold) < fold_size:
-			index = randrange(len(dataset_copy))
-			fold.append(dataset_copy.pop(index))
-		dataset_split.append(fold)
-	return dataset_split
-def evaluation_algorithm(dataset, algorithm, n_folds, min_size, sample_size, n_trees, n_features):
-    folds = cross_validation_split(dataset, n_folds)
+def evaluation_algorithm(dataset,  n_folds, min_size, n_trees, n_features):
+    #splitting the data into folds 
+    dataset_split = list()
+    dataset_copy = list(dataset)
+    fold_size = int(len(dataset) / n_folds)
+    for j in range(n_folds):
+        fold = list()
+        while len(fold) < fold_size:
+            index = randrange(len(dataset_copy))
+            fold.append(dataset_copy.pop(index))
+        dataset_split.append(fold)
+    folds = dataset_split
     scores = list()
+    #creating test and train datasets
     for fold in folds:
         train_set = list(folds)
         train_set.remove(fold)
@@ -164,25 +136,50 @@ def evaluation_algorithm(dataset, algorithm, n_folds, min_size, sample_size, n_t
             test_set.append(row_copy)
             #row_copy[-1] = 0
             row_copy[-1] = None
-        predicted = algorithm(train_set, test_set, min_size, sample_size, n_trees, n_features)
+        #getting the predictions for the algorithm 
+        predicted = random_forests(train_set, test_set, min_size, n_trees, n_features)
         #print(predicted)
-        actual = [row[-1] for row in fold]
+        actual = list()
+        for row in fold:
+            actual.append(row[-1])
+        correct = 0
+    #print(predicted)
+        for i in range(len(actual)):
+            if (actual[i] == predicted[i]):
+                correct += 1
         #print(actual)
-        accuracy = accuracy_metric(actual, predicted)
+        accuracy = correct / float(len(actual)) * 100.0
         scores.append(accuracy)
     return scores
+#driver code 
 if __name__ == "__main__":
-    filename = 'ml_data.csv'
-    dataset = load_csv(filename)
-    n_folds = 5
-    min_size = 1
-    sample_size = 1.0
+    dataset = []
+    with open('ml_data.csv', 'r') as f:
+        csv_reader = reader(f)
+        for row in csv_reader:
+            dataset.append(row)
+    fold_size = 6
+    min_termination_size = 1
+    sum_scores = 0.0
     n_features = int((len(dataset[0])-1)/2)
+    n_trees = 1
+    score = []
+    n_trees_list = [1,2,3,4,5,6,7,8,9,10]
+    mean_accuracies = list()
     #print(n_features)
-    for n_trees in [1]:
-        scores = evaluation_algorithm(dataset, create_random_forests, n_folds, min_size, sample_size, n_trees, n_features)
+    for n_trees in n_trees_list:
+        scores = evaluation_algorithm(dataset,fold_size, min_termination_size, n_trees, n_features)
         print('Trees: %d' % n_trees)
-        print('Scores: %s' % scores)
-        mean_accuracy = (sum(scores)/float(len(scores)))
-        print('Mean Accuracy: %.3f%%' % (mean_accuracy))
+        for score in scores:  
+            sum_scores = sum_scores + score
+        mean_accuracy = (sum_scores/6)
+        mean_accuracies.append(mean_accuracy)
+        sum_scores = 0 
+        print('Mean Accuracy: %.2f%%' % (mean_accuracy))
+    plt.plot(n_trees_list, mean_accuracies, color='black', linestyle='dashed', linewidth = 3)
+    plt.xlabel('Number of trees')
+    plt.ylabel('Accuracy')
+    plt.show()
 
+
+ 
